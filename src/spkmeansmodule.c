bf8dbf66320f708ff567575239294c37921a00e2
asaadsleman <asaadsleman@mail.tau.ac.il>
2021-09-05 14:37:38 +0300

    module in process

--- src/spkmeansmodule.c
+++ src/spkmeansmodule.c
@@ -1,19 +1,31 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include <spkmeans.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "spkmeans.h"
+
+
 
 
 static PyObject * spkmeans_WAMattrix(PyObject *self, PyObject *args)
 {
-    const char *command;
-    int sts;
+    PyObject* obj;
+    double** data;
+    double** adj;
+    int dim, i, dims[2];
 
-    if (!PyArg_ParseTuple(args, "s", &command))
+    if (!PyArg_ParseTuple(args, "Oi", &obj, &dim)){
+        printf("can't parse arguments - WAMatrix")
         return NULL;
-    sts = system(command);
-    return PyLong_FromLong(sts);
+    }
+    data = (double**)PyArray_DATA(obj);
+    Py_DECREF(obj);
+    adj = (double**)malloc(dim * sizeof(double*));
+    WAMatrix(data, dim, adj);
+    free(data);
+    dims[0] = dims[1] = dim;
+    return PyArray_SimpleNewFromData(2, dims, NPY_DOUBLE, adj);
+    
 }
 
 static PyObject * spkmeans_system(PyObject *self, PyObject *args)
