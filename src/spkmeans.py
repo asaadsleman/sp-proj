1b32ad577de949cd8f20cd671c3dc1a60dc5bd75
asaadsleman <asaadsleman@mail.tau.ac.il>
2021-09-11 21:16:40 +0300

    DONE!!!!

--- src/spkmeans.py
+++ src/spkmeans.py
@@ -3,16 +3,16 @@ import numpy as np
 from numpy.core.fromnumeric import shape
 from numpy.random import rand
 import pandas as pd
+from sklearn.metrics.pairwise import pairwise_distances
 import spkmeans
 
 
 def main():
     # Read user CMD arguments
-    k = sys.argv[1]
+    k = int(sys.argv[1])
     goal = sys.argv[2]
     filelines = pd.read_csv(sys.argv[3], header=None).astype('float64').to_numpy()
     data = filelines.tolist()
-    np.random.seed(0)
     # Interface with C
     perform(k, goal, data)
     
@@ -21,14 +21,17 @@ def main():
 
 
 def perform(k, objective, data):
+    dim = len(data)
     if(objective == "jacobi"):
-        jacobi = spkmeans.BuildJacobi(data)
-        ev = [data[i][i] for i in range(data.shape[0])]
+        ev = [0.0 for i in range(dim)]
+        res = spkmeans.BuildJacobi(data, ev, dim)
+        ev = res[0]
+        jacobi = res[1]
         print_array(ev)
         print("\n", end='')
         print_matrix(jacobi)
         return
-    dim = len(data)
+    
     Wam = spkmeans.WAMatrix(data, dim, len(data[0]))
     if objective == 'wam':
         print_matrix(Wam)
@@ -41,17 +44,23 @@ def perform(k, objective, data):
     if objective == 'lnorm':
         print_matrix(lap)
         return
-    ev = [0.0 for i in range(dim)]
-    jacobi = spkmeans.BuildJacobi(lap, ev, dim)
-    if objective == 'jacobi':
-        print(jacobi)
+    if objective != 'spk':
+        print("Invalid Input!", end='')
         return
+    ev = [0.0 for i in range(dim)]
+    result = spkmeans.BuildJacobi(lap, ev, dim)
+    ev = result[0]
+    jacobi = result[1]
     # build U and normalize it by row
-    u = spkmeans.BuildU(jacobi, ev,dim,  k)
+    u = spkmeans.BuildU(jacobi, ev, dim,  k)
+    # switch to np for center initialization
+    # update k first
+    k = len(u[0])
+    npu = np.array(u)
     # Calculate Initial Centroids as in HW2
-    centr_inf = centroids_init(u, k)
-    centr_indx = centr_inf[1].tolist()
-    centroids = centr_inf[0].tolist()
+    centr = centroids_init(npu, k)
+    centr_indx = centr[0]
+    centroids = centr[1]
     # Run Classification
     print_array(centr_indx)
     print("\n", end='')
@@ -62,24 +71,24 @@ def perform(k, objective, data):
 def print_array(arr):
     for i in range(len(arr)):
         num = arr[i]
-        if abs(num) < 0.0001:
+        if(num < 0.0 and abs(num) < 0.9e-4):
             num = 0.0
         if(i == (len(arr) - 1)):
-            print(f"{num:.4f}", end='')
+            print(f"{num:8.4f}", end='')
         else:
-            print(f"{num:.4f},", end='')
+            print(f"{num:8.4f},", end='')
 
 
 def print_matrix(mat):
     for i in range(len(mat)):
         for j in range(len(mat[0])):
             num = mat[i][j]
-            if abs(num) < 0.0001:
+            if(num < 0.0 and abs(num) < 0.9e-4):
                 num = 0.0
             if(j == (len(mat[0]) -1)):
-                print(f"{num:.4f}", end='')
+                print(f"{num:8.4f}", end='')
             else:
-                print(f"{num:.4f},", end='')
+                print(f"{num:8.4f},", end='')
         if(i != (len(mat) - 1)):
             print("\n", end='')
 
@@ -91,26 +100,26 @@ def centroids_init(data, k):
     # k : clust numbers
     # Returns centroids : numpy array
     #     Collection of k centroids as a numpy array.
-    npdata = np.array(data)
-    rangeind = [i for i in range(npdata.shape[0])]
-    rnd_ind = np.random.choice(rangeind)
-    centroids = [npdata[rnd_ind]]
-    cent_inds = [rnd_ind]
+    np.random.seed(0)
+    indxs = [0 for i in range(k)]
+    centroids = np.zeros((k, k))
+    rnd_ind = np.random.choice(data.shape[0])
+    centroids[0] = data[rnd_ind,:]
+    indxs[0] = rnd_ind
+    distances = pairwise_distances(data, [centroids[0]]).flatten()
 
-    for _ in range(1, k):
-        dist_sq = np.array([min([np.inner(cent-x,cent-x) for cent in centroids]) for x in data])
-        probs = dist_sq/dist_sq.sum()
-        accum_probs = probs.cumsum()
-        r = np.random.choice(accum_probs)
-        for j, p in enumerate(accum_probs):
-            if r < p:
-                i = j
-                break
-        centroids.append(npdata[i])
-        cent_inds.append(i)
-
-    return [np.array(centroids), np.np.array(cent_inds)]
-
+    for i in range(1,k):
+        summ = np.sum(distances)
+        ps = [distances[i]/summ for i in range(distances.shape[0])]
+        rnd_ind = np.random.choice(data.shape[0], size = 1, p=ps)
+        centroids[i] = data[rnd_ind,:]
+        indxs[i] = rnd_ind.item()
+        if i == k-1:
+            break
+        distances_new = pairwise_distances(data, [centroids[i]]).flatten()
+        distances = np.min(np.vstack((distances, distances_new)), axis = 0)
+    
+    return [indxs, centroids.tolist()]
 
 
 if __name__ == '__main__':
--- src/spkmeansmodule.c
+++ src/spkmeansmodule.c
@@ -1,8 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <numpy/ndarrayobject.h>
-#include <numpy/ndarraytypes.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,15 +8,9 @@
 
 #define assert__(x) for ( ; !(x) ; assert(x) )
 
-/* Util prototypes */
-double** parsePyMatrixToC(PyObject *input, int dim, int features);
-double* parsePyArrayToC(PyObject *input, int dim);
-PyObject * parseCMatrixToPy(double **input, int dim, int features);
-PyObject * parseCArrayToPy(double *input, int dim);
 
 /* ----------------- UTILITY FUNC - convert py list to c array ---------------------- */
 double* parsePyArrayToC(PyObject *input, int dim){
-    PyObject *line;
     double *data;
     int i;
     data = (double*)malloc(dim * sizeof(double));
@@ -125,8 +117,8 @@ static PyObject * spkmeans_WAMattrix(PyObject *self, PyObject *args)
 
 static PyObject * spkmeans_BuildLap(PyObject *self, PyObject *args)
 {
-    PyArrayObject *ddg, *wam, *lap;
-	double **din, **win, **mout;
+    PyObject *ddg, *wam, *lap;
+	double **din, **win, **lout;
 	int i, dim;
 	/* Parse tuples separately since args will differ between C fcns */
 	if (!PyArg_ParseTuple(args, "OOi", &ddg, &wam, &dim)) {
@@ -134,39 +126,36 @@ static PyObject * spkmeans_BuildLap(PyObject *self, PyObject *args)
         Py_INCREF(Py_None);
         return Py_None;
     }
-    din=parsePyMatrixToC(ddg);
-    win=parsePyMatrixToC(wam);
-    mout=(double**)malloc(n * sizeof(double*));
-    assert__(mout){
+    din=parsePyMatrixToC(ddg, dim, dim);
+    win=parsePyMatrixToC(wam, dim, dim);
+    lout=(double**)malloc(dim * sizeof(double*));
+    assert__(lout){
         free(din);
         free(win);
         printf("An Error Has Occured");
         Py_INCREF(Py_None);
         return Py_None;
     }
-	for ( i=0; i<n; i++)  {
-		mout[i]=malloc(n * sizeof(double));
-        assert__(mout[i]){
-            free(din);
-            free(win);
+	for ( i=0; i<dim; i++)  {
+		lout[i]=malloc(dim * sizeof(double));
+        assert__(lout[i]){
             printf("An Error Has Occured");
             Py_INCREF(Py_None);
             return Py_None;
         }
     }
-    mout = BuildLap(din, win, n);
-    lap = parseCMatrixToPy(mout, dim, dim);
+    lout = BuildLap(din, win, dim);
+    lap = parseCMatrixToPy(lout, dim, dim);
     free(din);
-    free(win);
-    free(mout);
+    free(lout);
     return lap;
 }
 
 static PyObject * spkmeans_BuildDDG(PyObject *self, PyObject *args)
 {
-    PyArrayObject *wam, *ddg;
+    PyObject *wam, *ddg;
 	double **win, **dout;
-	int i,n,m, dim;
+	int i, dim;
 	/* Parse tuples separately since args will differ between C fcns */
 	if (!PyArg_ParseTuple(args, "Oi", &wam, &dim)) {
         printf("An Error Has Occured");
@@ -183,8 +172,6 @@ static PyObject * spkmeans_BuildDDG(PyObject *self, PyObject *args)
     {
         dout[i] = (double*) malloc(dim * sizeof(double));
         assert__(dout[i]){
-            free(win);
-            free(dout);
             printf("An Error Has Occured");
         }
     }
@@ -197,7 +184,7 @@ static PyObject * spkmeans_BuildDDG(PyObject *self, PyObject *args)
 
 static PyObject * spkmeans_BuildJacobi(PyObject *self, PyObject *args)
 {
-    PyObject *lapin, *evin, *jacout;
+    PyObject *lapin, *evin, *jacout, *wrapper;
     int i, dim;
     double **jacobi, **lap;
 
@@ -208,8 +195,8 @@ static PyObject * spkmeans_BuildJacobi(PyObject *self, PyObject *args)
     assert__(jacobi){
         printf("An Error Has Occured");
     }
-    for(i = 0; i < n; i++){
-        jacobi[i] = (double*)malloc(dim * sizeof(double));
+    for(i = 0; i < dim; i++){
+        jacobi[i] = (double*)calloc(dim , sizeof(double));
         assert__(jacobi[i]){
             free(jacobi);
             printf("An Error Has Occured");
@@ -218,19 +205,22 @@ static PyObject * spkmeans_BuildJacobi(PyObject *self, PyObject *args)
     }
     lap = parsePyMatrixToC(lapin, dim, dim);
     BuildJacobi(dim, lap, jacobi);
-    for (i = 0; i < n; i++)
+    for (i = 0; i < dim; i++)
     {
         PyList_SetItem(evin, i, Py_BuildValue("d", lap[i][i]));
     }
+    wrapper = PyList_New(2);
+    PyList_SetItem(wrapper, 0, Py_BuildValue("O", evin));
     jacout = parseCMatrixToPy(jacobi, dim, dim);
+    PyList_SetItem(wrapper, 1, Py_BuildValue("O", jacout));
     free(jacobi);
     free(lap);
-    return  jacout;
+    return  Py_BuildValue("O", wrapper);
 }
 
 static PyObject * spkmeans_BuildU(PyObject *self, PyObject *args)
 {
-    PyArrayObject *jacin, *evin, *uout;
+    PyObject *jacin, *evin, *uout;
     double **jacobi, *eigV, **umat;
     int i, dim, k;
 
@@ -240,15 +230,13 @@ static PyObject * spkmeans_BuildU(PyObject *self, PyObject *args)
         return Py_None;
     }
     jacobi=parsePyMatrixToC(jacin, dim, dim);
-    eigV=parsePyArrayToC(ev, dim);
+    eigV=parsePyArrayToC(evin, dim);
     eigsrt(eigV, jacobi, dim);
     if(k == 0){
         k = eigengap(dim, eigV);
     }
     umat = (double**)malloc(dim * sizeof(double*));
     assert__(umat){
-        free(jacobi);
-        free(eigV);
         printf("An Error Has Occured");
         Py_INCREF(Py_None);
         return Py_None;
@@ -257,28 +245,25 @@ static PyObject * spkmeans_BuildU(PyObject *self, PyObject *args)
     {
         umat[i] = (double*)malloc(k * sizeof(double));
         assert__(umat[i]){
-            free(jacobi);
-            free(umat);
-            free(eigV);
             printf("An Error Has Occured");
             Py_INCREF(Py_None);
             return Py_None;
         }
     }
     BuildU(dim, k, jacobi, umat);
+    free(jacobi);
     NormalizeU(dim, k, umat);
     uout = parseCMatrixToPy(umat, dim, k);
-    free(jacobi);
     free(eigV);
     free(umat);
     return uout;
 }
 
 static PyObject * spkmeans_fit(PyObject *self, PyObject *args){
-    PyArrayObject *uin, *centin;
+    PyObject *uin, *centin;
     double **umat, **centmat;
-    int dim, k, i;
-    if(!PyArg_ParseTuple(args, "OOii", &u, &cent, &dim, &k)){
+    int dim, k;
+    if(!PyArg_ParseTuple(args, "OOii", &uin, &centin, &dim, &k)){
         printf("An Error Has Occured");
         Py_INCREF(Py_None);
         return Py_None;
@@ -287,8 +272,6 @@ static PyObject * spkmeans_fit(PyObject *self, PyObject *args){
     centmat = parsePyMatrixToC(centin, k, k);
     kmeans(umat, centmat, dim, k);
     centin = parseCMatrixToPy(centmat, k, k);
-    free(umat);
-    free(centmat);
     return centin;
 }
 
@@ -340,6 +323,5 @@ PyMODINIT_FUNC PyInit_spkmeans(void){
     if(!m){
         return NULL;
     }
-    import_array();
     return m;
 }
